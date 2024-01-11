a7861f0d5032842d06e933f530bb6826ec518b4c
Олександр Савченко <savasashok@MacBook-Pro-Oleksandr.local>
2024-01-11 18:14:11 +0200

    break and continue

--- WS3.js
+++ WS3.js
@@ -1,8 +1,17 @@
-let obj ={
-  name: 'Sanek',
-  age:14,
-  sayHI(){
-    console.log('hello');
+// let obj ={
+//   name: 'Sanek',
+//   age:14,
+//   sayHI(){
+//     console.log('hello');
+//   }
+// }
+// console.log(JSON.parse(JSON.stringify(obj)));
+
+
+for (let i = 0; i < 10; i++) {
+  if (i == 3) {
+    continue
   }
+  console.log(i);
+  
 }
-console.log(JSON.parse(JSON.stringify(obj)));

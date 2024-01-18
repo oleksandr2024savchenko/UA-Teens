6f5c6e8df0cfd75eabc965582ac1a8c055e9e956
oleksandr2024savchenko <savasashok@gmail.com>
2024-01-18 18:46:38 +0200

    session 4

--- WS3.js
+++ WS3.js
@@ -15,5 +15,19 @@
 
 //   }
 // }
-import os from "os";
-console.log('Платформа - ' + os.platform());
+// import os from "os";
+// console.log('Платформа - ' + os.platform());
+import express from "express";
+let app = express()
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>')
+});
+app.get('/google/:search' ,function(req, res) {
+  let search = req.params.search;
+  res.redirect('http://google.com/search?q=' + search)
+  
+})
+app.listen(3000, function() {
+  console.log('Екземпляр запущено через порт 3000');
+})

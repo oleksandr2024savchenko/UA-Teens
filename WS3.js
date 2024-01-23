b5a43e75f07708faf2dd2eea5a22e8e717084cf6
oleksandr2024savchenko <savasashok@gmail.com>
2024-01-23 21:51:44 +0200

    session 5

--- WS3.js
+++ WS3.js
@@ -17,17 +17,21 @@
 // }
 // import os from "os";
 // console.log('Платформа - ' + os.platform());
-import express from "express";
-let app = express()
+// import express from "express";
+// let app = express()
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>')
-});
-app.get('/google/:search' ,function(req, res) {
-  let search = req.params.search;
-  res.redirect('http://google.com/search?q=' + search)
+// app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>')
+// });
+// app.get('/google/:search' ,function(req, res) {
+//   let search = req.params.search;
+//   res.redirect('http://google.com/search?q=' + search)
   
-})
-app.listen(3000, function() {
-  console.log('Екземпляр запущено через порт 3000');
-})
+// })
+// app.listen(3000, function() {
+//   console.log('Екземпляр запущено через порт 3000');
+// })
+import {readFileSync, writeFileSync, appendFileSync} from "fs";
+// writeFileSync('test.txt', 'Test')
+// console.log(readFileSync('test.txt', 'utf-8'));
+appendFileSync('test.txt', '\n' + 'Testtt')

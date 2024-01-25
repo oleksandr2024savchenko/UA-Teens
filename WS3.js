280c7c52f7f500ec30eba8698eff7948183fe0c4
oleksandr2024savchenko <savasashok@gmail.com>
2024-01-25 18:30:10 +0200

    session 6

--- WS3.js
+++ WS3.js
@@ -1,37 +1,20 @@
-// let obj ={
-//   name: 'Sanek',
-//   age:14,
-//   sayHI(){
-//     console.log('hello');
-//   }
-// }
-// console.log(JSON.parse(JSON.stringify(obj)));
-// function newFunction() {
-//   for (let i = 0; i < 10; i++) {
-//     if (i == 3) {
-//       continue;
-//     }
-//     console.log(i);
+const button = document.getElementById('button')
+function sendInfo() {
+  let info = {
+    name: document.getElementById('name').value,
+    surname: document.getElementById('surname').value,
+    age: document.getElementById('age').value,
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value
+  }
+  console.log(info);
+  fetch('/registration', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(info)
+  })
+}
+button.addEventListener('click', sendInfo)
 
-//   }
-// }
-// import os from "os";
-// console.log('Платформа - ' + os.platform());
-// import express from "express";
-// let app = express()
-
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>')
-// });
-// app.get('/google/:search' ,function(req, res) {
-//   let search = req.params.search;
-//   res.redirect('http://google.com/search?q=' + search)
-  
-// })
-// app.listen(3000, function() {
-//   console.log('Екземпляр запущено через порт 3000');
-// })
-import {readFileSync, writeFileSync, appendFileSync} from "fs";
-// writeFileSync('test.txt', 'Test')
-// console.log(readFileSync('test.txt', 'utf-8'));
-appendFileSync('test.txt', '\n' + 'Testtt')
